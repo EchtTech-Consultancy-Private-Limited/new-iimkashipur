846d2df9f049d673091f2a8b9c906a32bf49ded2
brijesh-echttech <brijesh.sharma@echttech.com>
2024-08-23 12:21:37 +0530

    bgu fix

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'cms'), '_').'_session'
+        Str::slug(env('APP_NAME', 'iimkashipur'), '_').'_session'
     ),
 
     /*
